c0071e41cfd14533dbf575b7b7e5683fe5ac1177
UzoCode <akosabenedict@gmail.com>
2024-10-08 21:37:32 +0100

    ES6 Data Manipulation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 export default function updateStudentGradeByCity(arr, city, newGrades) {
-    return arr
-      .filter((els) => els.location === city)
-      .map((person) => {
-        const grades = newGrades.filter((els) => els.studentId === person.id);
-        const grade = grades.length ? grades[0].grade : 'N/A';
-        return {
-          ...person,
-          grade,
-        };
-      });
-  }
+  return arr
+    .filter((els) => els.location === city)
+    .map((person) => {
+      const grades = newGrades.filter((els) => els.studentId === person.id);
+      const grade = grades.length ? grades[0].grade : 'N/A';
+      return {
+        ...person,
+        grade,
+      };
+    });
+}
--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,6 @@
 export default function hasValuesFromArray(set, arr) {
-    for (const items of arr) {
-      if (!set.has(items)) return false;
-    }
-    return true;
+  for (const items of arr) {
+    if (!set.has(items)) return false;
   }
+  return true;
+}
